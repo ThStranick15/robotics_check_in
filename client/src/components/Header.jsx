428749ceea2803bee0d9212aac9d7cb9d78cb886
ThStranick <ThStranick@gmail.com>
2024-09-16 08:48:05 -0400

    admin button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,6 +8,7 @@ export default function Header() {
         <header className="relative text-4xl bg-blue-500 p-3 text-white text-center">
         {location.pathname !== '/' ? <NavLink to="/" className="absolute top-1 left-1 border p-2 my-1 mx-2 text-white bg-blue-600 hover:bg-blue-500 rounded text-xl">Back to Home</NavLink> : ""}
             <h1 className="">1234 Robotics Team Check In</h1>
+            {location.pathname !== '/admin' ? <NavLink to={'/admin'} className="absolute top-1 left-1 border p-2 my-1 mx-2 text-white bg-blue-600 hover:bg-blue-500 rounded text-xl">Go to Admin</NavLink> : ""}
         </header>
     )
 }
